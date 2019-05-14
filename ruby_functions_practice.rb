cb006cf1d1f8338368525173e18b2547959c45ff
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-05-14 16:49:27 +0100

    Jamie and Rory Testing

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,45 @@
+# def return_10
+#   return 10
+# end
+
+# def add(first_number, second_number)
+#   sum = first_number + second_number
+# return sum
+# end
+
+# def subtract(first_number, second_number)
+#   sum = first_number - second_number
+# return sum
+# end
+
+# def multiply(first_number, second_number)
+#   sum = first_number * second_number
+# return sum
+# end
+
+# def divide(first_number, second_number)
+#   sum = first_number / second_number
+#   return sum
+# end
+
+# def string_length
+#   string = "A string of length 21"
+#   return string.length
+# end
+
+# def join_string (string_1, string_2)
+#   string_1 = "Mary had a little lamb, "
+#   string_2 = "its fleece was white as snow"
+#   join_string = string_1 + string_2
+#
+#   return join_string
+# end
+#
+# def add_string_as_number(number_1, number_2)
+#   number_1 = "1".to_i
+#   number_2 = "2".to_i
+#   sum = number_1 + number_2
+#   return sum
+# end
+
+def number_to_full_month_name
